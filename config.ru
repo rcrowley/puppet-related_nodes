c6a68bf3e84fe9d9c6c3e0a12b3b6e8cbf64b7c5
Richard Crowley <r@rcrowley.org>
2012-04-16 16:28:56 +0000

    Update to Puppet 2.7.

--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require 'related_nodes'
 
-run RelatedNodes.new("var")
+run RelatedNodes.new("/var/lib/puppet/related_nodes")
--- lib/puppet/indirector/catalog/masterless_compiler.rb
+++ lib/puppet/indirector/catalog/masterless_compiler.rb
@@ -3,7 +3,7 @@ require 'puppet/resource/catalog'
 
 # Write the catalog to disk even if Puppet was invoked via `puppet apply`.
 # This is required in order to use RelatedNodes without a Puppet master.
-Puppet::Resource::Catalog.cache_class = :yaml
+Puppet::Resource::Catalog.indirection.cache_class = :yaml
 
 class Puppet::Resource::Catalog::MasterlessCompiler < Puppet::Resource::Catalog::Compiler
 end
