d45af021fb915ea1bc9ffc50d7bccc010998129d
Richard Crowley <r@rcrowley.org>
2013-06-26 23:17:44 +0000

    Fix #4506 in a pretty hacky way.

--- lib/puppet/indirector/catalog/masterless_compiler.rb
+++ lib/puppet/indirector/catalog/masterless_compiler.rb
@@ -1,5 +1,14 @@
 require 'puppet/indirector/catalog/compiler'
+require 'puppet/parameter/value'
 require 'puppet/resource/catalog'
+
+# Suppress Proc objects in Puppet::Parameter::Value YAML.
+# <http://projects.puppetlabs.com/issues/4506>
+class Puppet::Parameter::Value
+  def to_yaml_properties
+    [:@aliases, :@method, :@name]
+  end
+end
 
 # Write the catalog to disk even if Puppet was invoked via `puppet apply`.
 # This is required in order to use RelatedNodes without a Puppet master.
